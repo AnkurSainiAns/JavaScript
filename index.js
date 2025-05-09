6311bffd67b595dc7bcd3f389c6469064b7a1454
Ankur <an2811saini@gmail.com>
2025-05-09 23:03:59 +0530

    new code enter

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ let greeting = document.getElementById("thanks")
 console.log(greeting)
 
 function buyNow() {
-  greeting.textContent = "Thank you for your purchase!"}
+  greeting.textContent = "Thank you for your purchase!"
+} 
+// calculator
+let number1=5
+let number2=10
+document.getElementById("num1").textContent=number1
+document.getElementById("num2").textContent=number2
+let sum = document.getElementById("result")
+function add() {
+  let results = number1 + number2
+  sum.textContent = results
+}
+function subtract() {
+  let results = number1 - number2
+  sum.textContent = results
+}
+function multiply() {
+  let results = number1 * number2
+  sum.textContent = results
+}
+function divide() {
+  let results = number1 / number2
+  sum.textContent = results
+}
